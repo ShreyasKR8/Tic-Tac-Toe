15e08b363285450777e861281f694ba3524df34c
Shreyaskr8 <skrravi08@gmail.com>
2024-03-25 18:04:05 +0530

    basic command line game

--- game.js
+++ game.js
@@ -1,14 +1,60 @@
 const GameBoard = (function() {
     const gameBoard = [
-        ['X', 'O', 'X'],
-        ['X', 'X', 'O'],
-        ['O', 'O', 'X']
-    ];
-    const getBoard = () => gameBoard;
-    return {getBoard};
+        ['-', '-', '-'],
+        ['-', '-', '-'],
+        ['-', '-', '-']
+    ]; //private variable
+
+    const getBoard = () => {
+        gameBoard.forEach(element => {
+            console.log(element.join(" "));
+        });
+    };
+    const setBoard = (row, col, token) => {
+        gameBoard[row][col] = token;
+    };
+    return {getBoard, setBoard};
 }) ();
 
-let board = GameBoard.getBoard();
-board.forEach(element => {
-    console.log(element);
-});
+const Player = function () {
+    let name = '';
+    let token = '';
+    let currentPlayerIndex = 0;
+    const players = [];
+
+    const createPlayer = (playerName, playerToken) => {
+        const player = { name: playerName, token : playerToken}
+        players.push(player);
+        return player; //object
+    };
+
+    const switchPlayer = function () {
+        currentPlayerIndex = (currentPlayerIndex == 0) ? 1 : 0; //change player
+        return players[currentPlayerIndex];
+    };
+
+    return {createPlayer, switchPlayer};
+} ();
+
+Player.createPlayer('SKR', 'X');
+Player.createPlayer('MG', 'O');
+
+const GameController = (function() {
+    let input = 10;
+    let token = '';
+    while(input--)
+    {
+        const player = Player.switchPlayer();
+        GameBoard.getBoard(); //display the board;
+        let gameInput = window.prompt("Enter the row and column");
+        let gameInputArray = gameInput.split(' ');
+        GameBoard.setBoard(gameInputArray[0], gameInputArray[1], player.token);
+    }
+}) ();
+
+//
+// player1 = Player.createPlayer('SKR', 'X');
+// player2 = Player.createPlayer('MG', 'O');
+// console.log(player.token);
+// player = Player.switchPlayer();
+// console.log(player.token);
