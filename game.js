00332a7dedb64d654885e281903a784254eec7cc
Shreyaskr8 <skrravi08@gmail.com>
2024-03-31 12:15:58 +0530

    add win logic

--- game.js
+++ game.js
@@ -1,32 +1,22 @@
 const GameBoard = (function() {
     // const gameBoard = [ 'X', 'X', 'X', 'X', 'O', 'O', 'X', 'X', 'X' ]; //private variable
-    const gameBoard = [ '-', '-', '-', '-', '-', '-', '-', '-', '-' ]; //private variable
+    const gameBoard = [ '', '', '', '', '', '', '', '', '' ]; //private variable
     let i = 1;
     let output = '';
-    const getBoard = () => {
-        gameBoard.forEach(element => {
-            output += element; // concatenate the element to the output string
-            if (i % 3 === 0) { // if it's the third element, add a new line
-                output += '\n';
-            } else {
-                output += ' '; // add a space between elements
-            }
-            i++; // increment i
-        });
-        console.log(output);
-        output = '';
+    const getBoardState = () => {
+		let currentBoardState = [...gameBoard]; //shallow copy(one level) to avoid manipulation of private variable from outside.
+		return currentBoardState;
     };
     const markToken = (index, token) => {
         gameBoard[index] = token;
     };
 
     const clearBoard = () => {
-        for(let i = 0; i < 9; i++)
-        {
-            gameBoard[i] = '-';
+        for(let i = 0; i < 9; i++) {
+            gameBoard[i] = '';
         }
     }
-    return {getBoard, markToken, clearBoard};
+    return {getBoardState, markToken, clearBoard};
 }) ();
 
 const Player = function () {
@@ -41,7 +31,6 @@ const Player = function () {
 
     const switchPlayer = function () {
         currentPlayerIndex = (currentPlayerIndex == 0) ? 1 : 0; //change player
-        // return players[currentPlayerIndex];
     };
 
     const getCurrentPlayer = function() {
@@ -55,12 +44,30 @@ Player.createPlayer('SKR', 'X');
 Player.createPlayer('MG', 'O');
 
 const GameController = (function() {
+    const winCombos = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], //rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], //columns
+        [0, 4, 8], [2, 4, 7]             //diagonals 
+    ];
+	const checkForWin = (currentPlayer) => {
+		let currentBoardState = GameBoard.getBoardState();
+		for(let combo of winCombos) {
+            if(combo.every((index) => currentBoardState[index] == currentPlayer.token)) {
+                return true;
+            }
+        }
+        return false;
+	};
+
     const play = (boardIndex) => {
         const currentPlayer = Player.getCurrentPlayer();
         GameBoard.markToken(boardIndex, currentPlayer.token);
-        GameBoard.getBoard(); //display the board;
+		if(checkForWin(currentPlayer)) {
+            console.log(currentPlayer.name, "won");
+            return;
+        }
         Player.switchPlayer();
-    }
+    };
     
     return {play};
 }) ();
@@ -73,7 +80,7 @@ cells.forEach(cell => {
         const currentPlayerToken = Player.getCurrentPlayer().token;
         cell.textContent = currentPlayerToken;
         let cellNumber = cell.getAttribute("data-index");
-        GameController.play(cellNumber);
+        let outcome = GameController.play(cellNumber);
     });
 })
 
